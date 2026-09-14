f56aa440125d6a23f92ae69925cb28844ba652d7
Dannii10 <d.mendietaaj@gmail.com>
2026-09-14 16:46:56 -0500

    cambio del titulo de login , para probar que se guarda automaticamente en git

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if (estaLogueado()) header('Location: ' . redirigirSegunRol()); ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión - RentWheels</title>
+    <title>Iniciar sesión - RentWheels prueba</title>
     <link rel="stylesheet" href="css/style-global.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300;400;500;600;700&display=swap" rel="stylesheet">
     <style>
